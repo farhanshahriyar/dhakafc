db042265c6bbb287a8f79caca2ce78e5abd9b834
farhanshahriyar <farhan.abir9999@gmail.com>
2022-11-28 19:55:28 +0600

    added about us

--- assets/main.js
+++ assets/main.js
@@ -8,10 +8,29 @@ menu.onclick = () => {
     navbar.classList.toggle('acitve');
 }
 
+
+//scroll spy
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .nav a');
+
 // search box
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('acitve');
+
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.Attribute('id');
+
+        if(top => offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header .nav a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
 }
 
 document.querySelector('#search-icon').onclick = () => {
